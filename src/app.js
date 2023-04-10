639f5a7277900c019bb12b17599bc728e514491b
leticia-chijo <leticia.chijo@gmail.com>
2023-04-10 20:31:47 -0300

    Bonus 1: validações nos POSTs

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ const users = []    // { username: "bobesponja", avatar: "www.imagem.com" }
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body
 
+    if (!username || typeof userame !== "string" || !avatar || typeof avatar !== "string") {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
     users.push({ username, avatar })
     console.log(users)
     res.send("OK")
@@ -21,6 +25,10 @@ app.post("/sign-up", (req, res) => {
 
 app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body
+
+    if (!username || typeof userame !== "string" || !tweet || typeof tweet !== "string") {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
 
     // find => retornar undefined se não achar OU o objeto do usuário se achar
     const userExists = users.find((user) => user.username === username)
@@ -32,12 +40,14 @@ app.post("/tweets", (req, res) => {
 })
 
 app.get("/tweets", (req, res) => {
-    const completeTweets = tweets.map((tweet) => {
+    const tenTweets = tweets.slice(-10)
+
+    const completeTweets = tenTweets.map((tweet) => {
         const user = users.find((u) => u.username === tweet.username)
         return { ...tweet, avatar: user.avatar }
     })
 
-    res.send(completeTweets.slice(-10))
+    res.send(completeTweets)
 })
 
 const PORT = 5000
