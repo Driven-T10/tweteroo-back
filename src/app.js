98536a61cb1f5d3c05e8959cb549a5289c103f35
leticia-chijo <leticia.chijo@gmail.com>
2023-04-10 20:50:49 -0300

    Bônus 3: GET /tweets/:username

--- src/app.js
+++ src/app.js
@@ -50,5 +50,18 @@ app.get("/tweets", (req, res) => {
     res.send(completeTweets)
 })
 
+app.get("/tweets/:username", (req, res) => {
+    const { username } = req.params
+
+    const filteredTweets = tweets
+        .filter((tweet) => tweet.username === username)
+        .map((tweet) => {
+            const user = users.find((u) => u.username === tweet.username)
+            return { ...tweet, avatar: user.avatar }
+        })
+
+    res.send(filteredTweets)
+})
+
 const PORT = 5000
 app.listen(PORT, () => console.log(`Rodando servidor na porta ${PORT}`))
