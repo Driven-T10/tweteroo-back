0c7814a10dcd87ffb1960b9d8c31d857c9a24259
leticia-chijo <leticia.chijo@gmail.com>
2023-04-11 14:36:09 -0300

    Bônus 5: user vindo pelo header

--- src/app.js
+++ src/app.js
@@ -23,24 +23,24 @@ app.post("/sign-up", (req, res) => {
 })
 
 app.post("/tweets", (req, res) => {
-    const { username, tweet } = req.body
+    const { tweet } = req.body
+    const { user } = req.headers
 
-    if (!username || typeof username !== "string" || !tweet || typeof tweet !== "string") {
+    if (!user || typeof user !== "string" || !tweet || typeof tweet !== "string") {
         return res.status(400).send("Todos os campos são obrigatórios!")
     }
 
     // find => retornar undefined se não achar OU o objeto do usuário se achar
-    const userExists = users.find((user) => user.username === username)
+    const userExists = users.find((u) => u.username === user)
 
     if (!userExists) return res.status(401).send("UNAUTHORIZED")
 
-    tweets.push({ username, tweet })
+    tweets.push({ username: user, tweet })
     res.status(201).send("OK")
 })
 
 app.get("/tweets", (req, res) => {
     const page = Number(req.query.page)
-    console.log(page)
 
     if (req.query.page && (isNaN(page) || page < 1)) {
         return res.status(400).send("Informe uma página válida!")
@@ -53,7 +53,7 @@ app.get("/tweets", (req, res) => {
 
     if (page) {
         const limit = 10
-        const start = (page - 1 ) * limit
+        const start = (page - 1) * limit
         const end = page * limit
 
         return res.send(completeTweets.slice(start, end))
